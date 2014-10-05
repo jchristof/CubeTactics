af48d7be021072051cc75061b4cac110caf81bd2
Jon Martin <jchristofmartin@gmail.com>
2014-10-04 23:49:22 -0500

    added fmod added new title screen ui

--- Assets/GUI/LevelMenuGUI.cs
+++ Assets/GUI/LevelMenuGUI.cs
@@ -7,18 +7,26 @@ using Assets.Game;
 
 public class LevelMenuGUI : MonoBehaviour {
 
+    GUIStyle myButtonStyle;
+    void Start() {
+
+    }
+
     void OnGUI() {
         int yPos = 0;
-
+        myButtonStyle = new GUIStyle(GUI.skin.button);
+        myButtonStyle.fontSize = 24;
+        Font myFont = (Font)Resources.Load("Fonts/Mario-Kart-DS", typeof(Font));
+        myButtonStyle.font = myFont;
         foreach (var e in Enum.GetValues(typeof(LevelName))) {
-            yPos += 50;
-            if (GUI.Button(new Rect(20, yPos, 300, 30), e.ToString())) {
+            yPos += 90;
+            if (GUI.Button(new Rect(20, yPos, 500, 75), e.ToString(), myButtonStyle)) {
                 Application.LoadLevel("scenedemo");
                 Game.levelToLoad = (LevelName)e;
             }
         }
-        yPos += 50;
-        if (GUI.Button(new Rect(20, yPos, 300, 30), "Quit"))
+        yPos += 90;
+        if (GUI.Button(new Rect(20, yPos, 500, 75), "Quit", myButtonStyle))
                 Application.Quit();
     }
 }
---
+++ Assets/LoadscreenCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Movement;
+using System.Collections.Generic;
+
+public class LoadscreenCube : MonoBehaviour {
+
+    Assets.Movement.Interpolate.Function _ease;
+    IEnumerator _sequence;
+	// Use this for initialization
+	void Start () {
+        _ease = Interpolate.Ease(Interpolate.EaseType.EaseInOutSine);
+	}
+
+    Vector3 _rotation;
+	// Update is called once per frame
+	void Update () {
+        if (_sequence != null && _sequence.MoveNext()) {
+            transform.rotation = Quaternion.Euler((Vector3)_sequence.Current);
+        }
+        else {
+            _rotation = new Vector3(Random.value * 180, Random.value * 180, Random.value * 180);
+            _sequence = Interpolate.NewEase(_ease, transform.rotation.eulerAngles, _rotation, 5.0f);
+        }
+	}
+}
