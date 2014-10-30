7c104bae1f951f88e6a17237fffdb5f222772bf2
Jon Martin <jchristofmartin@gmail.com>
2014-10-29 22:04:55 -0500

    expression eval

---
+++ Assets/Script/Commands/VarAssignmentCommand.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.Commands {
+    [JsonConverter(typeof(CommandConverter))]
+    public class VarAssignmentCommand : Command {
+
+        public string Target { set; get; }
+        public string Expression { set; get; }
+
+#if OBJECTEDITOR 
+        [Browsable(false)]
+        public override Identifier Identifier { get; set; }
+
+        [Browsable(false)]
+        public override object SecondaryInfo {
+            get { return null; }
+        }
+#endif
+        public override void Execute() {
+            CompositionRoot.RunOnMainThread(() => {
+                CompositionRoot.ScriptExpression.Assignment(Target, Expression);
+            });
+        }
+    }
+}
---
+++ Assets/Script/Commands/VarDeclareCommand.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Editor;
+using Assets.Script.Expressions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Script.Commands {
+    [JsonConverter(typeof(CommandConverter))]
+    public class VarDeclareCommand : Command {
+#if OBJECTEDITOR 
+        public VarDeclareCommand() {
+            Variable = new List<ScriptValueDecl>();
+        }
+#endif
+        public List<ScriptValueDecl> Variable { set; get; }
+#if OBJECTEDITOR 
+        [Browsable(false)]
+        public override Identifier Identifier { get; set; }
+
+        [Browsable(false)]
+        public override object SecondaryInfo {
+            get { return null; }
+        }
+#endif
+        public override void Execute() {
+            CompositionRoot.RunOnMainThread(() => {
+                foreach (ScriptValueDecl variable in Variable) {
+                    CompositionRoot.ScriptExpression.AddVariable(variable.Name, variable.Value);
+                    DebugLog.Log("Expressions", "New variable: " + variable.Name + " value: " + variable.Value);
+                }
+            });
+        }
+    }
+}
