0847234cd708dc2439c83439ad699a5070458cb3
Jon Martin <jchristofmartin@gmail.com>
2014-10-29 21:53:55 -0500

    adding custom debug logger

---
+++ Assets/Editor/DebuggerEditor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor {
+    public class DebuggerEditor : EditorWindow {
+         enum bools{
+         False,
+         True
+     }
+     
+     string newTag = "";
+     bools newBools = bools.False;
+     bool newValue = false;
+     
+     [MenuItem("DebugLog/log")]
+     public static void Init(){
+         GetWindow<DebuggerEditor>();
+     }
+     
+     void OnEnable(){
+         DebugLog.tags.Add("Scripts",true);
+         DebugLog.tags.Add("Expressions",true);
+     }
+     
+     void OnGUI(){
+         foreach(KeyValuePair<string,bool> kvp in DebugLog.IteratableDictionary()){
+             GUILayout.BeginHorizontal();
+             EditorGUI.BeginChangeCheck();
+             newValue = GUILayout.Toggle(DebugLog.tags[kvp.Key], kvp.Key);
+             if(EditorGUI.EndChangeCheck()){
+                 DebugLog.SetTag(kvp.Key,newValue);
+             }
+             GUILayout.EndHorizontal();
+         }
+         
+         GUILayout.BeginHorizontal();
+         newTag = GUILayout.TextField(newTag);
+         newBools = (bools)EditorGUILayout.EnumPopup(newBools);
+         GUILayout.EndHorizontal();
+         if(GUILayout.Button("Add"))
+             DebugLog.SetTag(newTag,bool.Parse(newValue.ToString()));
+     }
+ }
+}
---
+++ Assets/Tools/DebugLog.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Editor {
+
+ 
+ public static class DebugLog {
+ 
+ #if UNITY_EDITOR
+     public static Dictionary<string,bool> tags = new Dictionary<string,bool>();
+ #endif
+     
+     public static void Log(string tag, string message){
+ #if UNITY_EDITOR
+         if(!tags.ContainsKey(tag)){
+             Debug.LogWarning("Tag "+ tag +" not found in MyDebugger's TagDictionary! Adding it...");
+             tags.Add(tag,true);
+         }
+         if(tags[tag] == true)
+             Debug.Log(message);
+ #endif 
+     }
+     
+     public static Dictionary<string,bool> IteratableDictionary(){
+ #if UNITY_EDITOR
+         return new Dictionary<string, bool>(tags);
+ #endif
+     }
+     
+     public static void SetTag(string tag, bool flag){
+ #if UNITY_EDITOR
+         if(tags.ContainsKey(tag))
+             tags[tag]= flag;
+         else
+             tags.Add(tag,flag);
+ #endif
+     }
+     
+ }
+    
+}
