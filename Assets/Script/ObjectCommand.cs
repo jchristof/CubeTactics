efab7667877a645e09d96a364ea8e90b09f6d7aa
Jon Martin <jchristofmartin@gmail.com>
2014-10-29 22:02:12 -0500

    new commands

--- Assets/Script/ObjectCommand.cs
+++ Assets/Script/ObjectCommand.cs
@@ -21,5 +21,8 @@ namespace Assets.Script {
         PlayerInputState,
         LevelExit,    
         LevelCompletionShow,
+        EvaluateExpression,
+        VarAssignment,
+        VarDecalare,
     }
 }
