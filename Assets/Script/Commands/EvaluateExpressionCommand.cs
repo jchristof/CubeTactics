e2098bf9d8cfd43933294711c34878b1199c96bc
Jon Martin <jchristofmartin@gmail.com>
2014-10-29 22:01:12 -0500

    new evaluate expression command

---
+++ Assets/Script/Commands/EvaluateExpressionCommand.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.Commands {
+    [JsonConverter(typeof(CommandConverter))]
+    public class EvaluateExpressionCommand : Command {
+
+        public string False { get; set; }
+        public string True { get; set; }
+        public string Expression { get; set; }
+#if OBJECTEDITOR 
+        [Browsable(false)]
+        public override Identifier Identifier { get; set; }
+
+        [Browsable(false)]
+
+        public override object SecondaryInfo {
+            get { return null; }
+        }
+#endif
+        public override void Execute() {
+            CompositionRoot.RunOnMainThread(() => {
+                if (CompositionRoot.ScriptExpression.Evaluate(Expression))
+                    CompositionRoot.ScriptExecutor.Execute(True);
+                else
+                    CompositionRoot.ScriptExecutor.Execute(False);
+            });
+        }
+    }
+}
